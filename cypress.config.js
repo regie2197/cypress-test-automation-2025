36bf6afcdb062b3df7cbd8b22ded154ff13942b0
Reginald <reginald2197@gmail.com>
2025-04-15 15:05:02 +0800

    fix: cypress config and update package json and update cypress yml to apply merge report

--- cypress.config.js
+++ cypress.config.js
@@ -1,45 +1,44 @@
-//npx cypress run --record --key adbc781d-1e72-4019-9bd0-a01ef834246e
 const { defineConfig } = require("cypress");
-const dotenv = require('dotenv')
-const { beforeRunHook, afterRunHook } = require('cypress-mochawesome-reporter/lib');
+const dotenv = require("dotenv");
+const {
+  beforeRunHook,
+  afterRunHook,
+} = require("cypress-mochawesome-reporter/lib");
 
-dotenv.config()
+dotenv.config();
 
 module.exports = defineConfig({
   projectId: "tqyggs",
   experimentalStudio: true,
   defaultCommandTimeout: 6000,
-  //testIsolation: false,
   retries: 3,
-  reporter: 'cypress-mochawesome-reporter',
+  reporter: "cypress-mochawesome-reporter",
   reporterOptions: {
     charts: true,
-    reportFilename: 'regression-test-report',
-    reportDir: 'cypress/reports/individual',
-    reportPageTitle: 'Regression Testing Report',
+    reportFilename: "regression-test-report", // Static fallback name
+    reportDir: "cypress/reports/individual",
+    overwrite: false,
+    reportPageTitle: "Regression Testing Report",
     embeddedScreenshots: true,
     inlineAssets: true,
     saveAllAttempts: false,
     json: true,
-    html: true,
+    html: false,
   },
-  env:{
+  env: {
     dev: process.env.DEV_URL,
     qa: process.env.QA_URL,
     uat: process.env.UAT_URL,
   },
   e2e: {
-
-    //testIsolation: false,
-    //baseUrl: "https://qa-practice.netlify.app",
     setupNodeEvents(on, config) {
-      on('before:run', async (details) => {
-        console.log('override before:run');
-        console.log('Running tests');
+      on("before:run", async (details) => {
+        console.log("override before:run");
         await beforeRunHook(details);
       });
-      on('after:run', async () => {
-        console.log('override after:run');
+
+      on("after:run", async () => {
+        console.log("override after:run");
         await afterRunHook();
       });
     },
